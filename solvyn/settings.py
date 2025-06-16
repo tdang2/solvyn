46228759f10d3a3c8b96dfe1f5e3e4b89aa948a8
Tri Dang <quangtrister@gmail.com>
2025-06-15 22:07:10 -0400

    added styles and material ui

--- solvyn/settings.py
+++ solvyn/settings.py
@@ -136,6 +136,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'base', 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
